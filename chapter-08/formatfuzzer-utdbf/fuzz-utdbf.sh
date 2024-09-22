65cba291bf069a8afb187a2eb7975d57f5d9f384
Eugene Lim <eugenelim@outlook.com>
2024-09-23 00:38:43 +0800

    fix: correct relative paths for utdbf script

--- chapter-08/formatfuzzer-utdbf/fuzz-utdbf.sh
+++ chapter-08/formatfuzzer-utdbf/fuzz-utdbf.sh
@@ -4,6 +4,6 @@ while true
 do
   ./dbf-fuzzer fuzz test.dbf 2>/dev/null
   # run utdbf for maximum 1 second on test case and exit
-  timeout 1 ../utdbf/utdbf ./test.dbf <<< "0" >/dev/null
+  timeout 1 ./utdbf ./test.dbf <<< "0" >/dev/null
   test $? -gt 127 && break
 done
